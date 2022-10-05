4091e99cd1190b65976da7643646da0596f96e5c
Marine Darbinyan <darbimar@mail.ru>
2022-10-05 15:48:47 +0700

    Add slider

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.testimonial__clients').slick({
+        arrows:false,
+        dots: true,
+        adaptiveHeight: true,
+        slidesToShow:2,
+        slidesToScroll:2,
+        speed:1000,
+        easing:'linear',
+        initialSlide:0,
+        autoplay:true,
+        pauseOnFocus:true,
+    });
+});
