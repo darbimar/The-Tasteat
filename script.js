5ec659a33e617a9b6e5346b069a8741dc6ce2b04
Marine Darbinyan <darbimar@mail.ru>
2022-10-26 19:51:47 +0700

    Add burger button

--- script.js
+++ script.js
@@ -33,4 +33,15 @@ $(document).ready(function(){
                 }
             ]
     });
-n});
+});
+
+
+const menuIcon = document.querySelector('.menu-icon'),
+    header = document.querySelector('header'),
+    body = document.querySelector('body');
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('menu-icon--active');
+    header.classList.toggle('header--mobile');
+    body.classList.toggle('no-scroll');
+});
